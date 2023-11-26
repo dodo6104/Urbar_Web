082408835ae0a09efe22fe3c03e9bcc07bce1748
dodo6104 <dodo6104@gmail.com>
2023-11-26 13:52:41 +0100

    Test web servera

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 oznamy = [
-    {"nazov": "Pozvánka na Oslavy Dňa Zeme", "text": """Nezmeškajte prednášku o ochrane prírody, ktorá sa uskutoční v miestnom kultúrnom dome dňa 5. júla.""", "date": "28.11.2023"},
+    {"nazov": "Pozváfnka na Oslavy Dňa Zeme", "text": """Nezmeškajte prednášku o ochrane prírody, ktorá sa uskutoční v miestnom kultúrnom dome dňa 5. júla.""", "date": "28.11.2023"},
     {"nazov": "Pozvánka na Oslavy Dňa Zeme", "text": "Plánujeme výsadbu nových stromov v oblasti severného parku. Všetci sú vítaní sa zúčastniť 22. novembra od 9:00.", "date": "28.11.2023"},
     {"nazov": "Zber Odpadu v Parku", "text": "Zorganizujeme zber odpadu v mestskom parku tento víkend. Pridajte sa k nám a pomôžte udržať náš park čistý!", "date": "28.11.2023"},
     {"nazov": "Pozvánka na Oslavy Dňa Zeme", "text": "Srdečne vás pozývame na oslavy Dňa Zeme, ktoré sa uskutočnia 22. apríla na hlavnom námestí.", "date": "28.11.2023"},
